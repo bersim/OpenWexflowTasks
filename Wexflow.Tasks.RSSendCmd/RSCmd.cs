c7547e838bcb6edad7106e5b0f346f78c48b04b8
bersim <vvk.kos@gmail.com>
2017-08-25 14:07:01 +0500

    Add xml doc for Wexflow Tasks

---
+++ Wexflow.Tasks.RSSendCmd/RSCmd.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Scada.Data.Models;
+
+namespace Wexflow.Tasks.RSSendCmd
+{
+	public class RSCmd
+	{
+		public int IDSrv { get; private set;}
+		public Command Cmd { get; set;}
+
+		public RSCmd (int idsrv)
+		{
+			IDSrv = idsrv;
+			Cmd = new Command ();
+		}
+	}
+}
+
---
+++ Wexflow.Tasks.RSSendCmd/RSSendCmd.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Threading;
+using Wexflow.Core;
+using Scada.Data.Models;
+using Scada.Data.Tables;
+
+
+namespace Wexflow.Tasks.RSSendCmd
+{
+	public class RSSendCmd : Task
+	{
+
+		public List<RSCmd> rscmds { get; private set;}
+		public XElement[] XCmds { get; private set;}
+
+		public int Cnt { get; private set;}
+
+		public RSSendCmd (XElement xe,Workflow wf) : base(xe,wf)
+		{
+			//XCmds = GetXSettings ("rscmd");
+
+
+			rscmds = new List<RSCmd> ();
+
+
+			
+		}
+
+		public override TaskStatus Run ()
+		{
+			Info ("Start RSSendCmd task");
+
+			bool success = false;
+
+			Cnt++;
+
+			InfoFormat ("Value Cnt: {0}", Cnt);
+
+			try
+			{
+				
+			}
+			catch(ThreadAbortException)
+			{
+				throw;
+			}
+			catch(Exception ex)
+			{
+				ErrorFormat ("Ann error RSSendCmd. Error: {0}", ex.Message);
+			}
+
+			Info ("Task RSSendCmd finished.");
+			return new TaskStatus(Status.Success,success);
+		}
+	}
+}
+
